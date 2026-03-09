7da46fdd49dccdb46cf76f8ab91128c650345b40
Arunkrishnacm <111333698+Arunkrishnacm@users.noreply.github.com>
2026-03-09 21:14:16 +0530

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://main-project-entri-frontend-1.onrender.com",
   credentials: true
 }));
 
