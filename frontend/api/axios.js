7c0db36e5c8714bb9aea6fde4d4807039fe332a5
Arunkrishnacm <111333698+Arunkrishnacm@users.noreply.github.com>
2026-03-14 15:52:59 +0530

    Update axios.js

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://main-project-entri-backend.onrender.com/api",
+  baseURL: "https://main-project-entri-backend.onrender.com",
 });
 
 export default instance;
