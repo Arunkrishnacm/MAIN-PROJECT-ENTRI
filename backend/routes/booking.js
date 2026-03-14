6d7b031603075c28284ecf3ae538232e477fca8e
Arunkrishnacm <111333698+Arunkrishnacm@users.noreply.github.com>
2026-03-14 15:24:49 +0530

    Delete backend/routes/booking.js

--- backend/routes/booking.js
+++
@@ -1,9 +0,0 @@
-
-const router = express.Router();
-const { verifyToken } = require('../middleWare/authMiddleware');
-const { authorizeRoles } = require('../middleWare/roleMiddleware');
-const { createBooking, getUserBookings } = require('../controllers/bookingController');
-
-router.post('/create', verifyToken, authorizeRoles('user'), createBooking);
-router.get('/bookings', verifyToken, authorizeRoles('user'), getUserBookings);
-export default router;
